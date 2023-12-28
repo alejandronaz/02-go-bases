3c9f2529c84d694759d5ea3f45e0005fdd5ef263
Alejandro N. Rodriguez <alejandronazareno.rodriguez@mercadolibre.com>
2023-12-28 14:04:27 -0300

    add exercise 2

---
+++ 01-Variables-DT-Operadores/02-clima/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var temperatura, humedad, presion float64 = 32.0, 0.5, 1.0
+
+	fmt.Printf("Temperatura = %f\n, Humedad = %f\n, Presion = %f\n", temperatura, humedad, presion)
+
+}
